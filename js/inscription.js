bce6d67f0b3e2cf3eeef50e31b290e6de32a5b81
Kraitze <jacquelet.guillaume@gmail.com>
2017-05-02 01:28:13 +0200

    Ajout de inscription.js + modification inscription.html

---
+++ js/inscription.js
@@ -0,0 +1,17 @@
+// Fichier JavaScript pour le formulaire d'inscription
+
+$(document).ready(function(){
+
+      console.log("Fichier JS Inscription.php chargé.");
+
+      // Empêcher les messages d'erreurs de formulaire de s'afficher
+      $('#nameError').css("visibility", "hidden");
+      $('#firstNameError').css("visibility", "hidden");
+      $('#emailError').css("visibility", "hidden");
+      $('#passwordError').css("visibility", "hidden");
+
+      console.log("Messages d'erreur(s) effacés avec succès");
+
+      
+
+});
